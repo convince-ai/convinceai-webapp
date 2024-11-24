a7cd4fd9c12b53314b22beac09fe31f2197c050f
breCosta <brenda.costa@unifesp.br>
2024-11-24 16:22:57 -0300

    Adicionando pagina de Login

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom'; ///client
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import GlobalStyles from './styles/GlobalStyles';
 import Layout from './Components/Layout';
 import Navbar from './Components/Navbar';
-
-
+import Login from './Components/Login/login';
 
 function App() {
   return (
-    <>
-      <Layout>
-        <Navbar />
-      </Layout>
+    <Router>
       <GlobalStyles />
-    </>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route
+          path="/app"
+          element={
+            <Layout>
+              <Navbar />
+            </Layout>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
